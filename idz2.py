7b96f9ea16b84325b50ea7a07c6049152c43a874
DaniilMedy <135615674+DaniilMedy@users.noreply.github.com>
2023-06-06 17:35:10 +0300

    Create idz2.py

---
+++ idz2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+if __name__ == '__main__':
+    a = float(input("Введите первое число: "))
+    b = float(input("Введите второе число: "))
+    c = float(input("Введите третье число: "))
+
+    if 0 < a < 1:
+        print(a)
+    if 0 < b < 1:
+        print(b)
+    if 0 < c < 1:
+        print(c)
+
